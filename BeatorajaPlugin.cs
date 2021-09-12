1e7720032b08d4bcce730e626c8dea24abb304f6
SackMagiclight <20876249+SackMagiclight@users.noreply.github.com>
2021-09-12 18:26:07 +0900

    first commit

---
+++ BeatorajaPlugin.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using Plugin;
+using SitePlugin;
+using System.ComponentModel.Composition;
+using System.IO.Pipes;
+using System.Text;
+
+namespace MCV_beatorajaPlugin
+{
+    [Export(typeof(IPlugin))]
+    public class BeatorajaPlugin : IPlugin, IDisposable
+    {
+
+        public string Name { get { return "beatoraja連携"; } }
+        public string Description { get { return "beatoraja連携"; } }
+        public IPluginHost Host { get; set; }
+
+        // named pipe
+        private NamedPipeServerStream pipeServer;
+
+        public virtual void OnLoaded()
+        {
+            pipeServer = new NamedPipeServerStream("beatoraja", PipeDirection.Out, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
+            Console.WriteLine("[PIPE SERVER] thread created");
+            pipeServer.BeginWaitForConnection(ConnectCallback, null);
+        }
+
+        public void OnClosing()
+        {
+            pipeServer.Close();
+        }
+
+        public void OnMessageReceived(ISiteMessage message, IMessageMetadata messageMetadata)
+        {
+            if (pipeServer.IsConnected)
+            {
+                var (name, text) = PluginCommon.Tools.GetData(message);
+                if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(text))
+                {
+                    byte[] sendMessage = Encoding.UTF8.GetBytes(text.ToString() + "\n");
+                    pipeServer.BeginWrite(sendMessage, 0, sendMessage.Length, WriteCallback, null);
+                }
+                
+            }
+        }
+
+        public void OnTopmostChanged(bool isTopmost)
+        {
+
+        }
+
+        public void ShowSettingView()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+
+        }
+
+        private void ConnectCallback(IAsyncResult result)
+        {
+            Console.WriteLine("[PIPE SERVER] Client Connected");
+            pipeServer.EndWaitForConnection(result);
+            if (pipeServer.IsConnected)
+            {
+                byte[] message = Encoding.UTF8.GetBytes("START\n");
+                pipeServer.BeginWrite(message, 0, message.Length, WriteCallback, null);
+            }
+        }
+
+        private void WriteCallback(IAsyncResult ar)
+        {
+            pipeServer.EndWrite(ar);
+        }
+
+    }
+}
